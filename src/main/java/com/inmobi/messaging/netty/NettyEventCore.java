516f5078a8742fecf2c7f5269ce497be3867ce2f
Arvind Jayaprakash <work@anomalizer.net>
2011-06-28 10:59:33 +0530

    bugfix: supports multiple instances of the client (netty loop setup factored out)

---
+++ src/main/java/com/inmobi/messaging/netty/NettyEventCore.java
@@ -0,0 +1,54 @@
+package com.inmobi.messaging.netty;
+
+import org.jboss.netty.channel.socket.*;
+import org.jboss.netty.channel.socket.nio.*;
+
+import java.util.concurrent.*;
+
+class NettyEventCore {
+    private static NettyEventCore ourInstance = new NettyEventCore();
+
+    private ClientSocketChannelFactory factory = null;
+    private int leases = 0;
+
+    public static NettyEventCore getInstance() {
+        return ourInstance;
+    }
+
+    private NettyEventCore() {
+    }
+
+    /**
+     * Get a handle to the netty event loop
+     *
+     * The NIO handlers are setup lazily
+     * @return an NIO handler
+     */
+    public synchronized ClientSocketChannelFactory getFactory() {
+        if(factory == null) {
+            factory = new NioClientSocketChannelFactory(
+                    Executors.newCachedThreadPool(),
+                    Executors.newCachedThreadPool());
+        }
+        leases++;
+        return factory;
+    }
+
+    /**
+     * Application indicating that it no longer needs this
+     *
+     * In real world, assume that this will not be released,
+     * we are being nice and try and release it.
+     */
+    public synchronized void releaseFactory() {
+        if(factory != null) {
+            leases--;
+            if(leases == 0) {
+                factory.releaseExternalResources();
+                factory = null;
+            }
+        } else {
+            //WTF! releasing what you did not take
+        }
+    }
+}
